711c4e4d12aa90be4073b59307e5512fc3c0b5b5
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2018-09-12 19:49:22 -0400

    fixed s3

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -7,5 +7,5 @@ class Spreadsheet(object):
         with open("beta_testers.txt",'a') as data:
             entry = values['name1'], values['email1'],values['gender'],values['FavoriteShoe']
             data.write("\nentry: {}".format( entry))
-            s3.meta.client.upload_file('/tmp/beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
+            s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
             print("operation complete")
