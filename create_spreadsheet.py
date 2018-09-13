ec997234920b04fac68f80996053bc3e31343b43
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2018-09-12 21:54:59 -0400

    d

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -8,6 +8,7 @@ class Spreadsheet(object):
         with open("beta_testers.txt",'w+') as data:
             entry = values['name1'], values['email1'],values['gender'],values['FavoriteShoe']
             self.entries[self.count] = entry
+            print(self.entries)
             data.write("\nentry {}: {}".format(self.count,self.entries[self.count]))
             self.count += 1
             s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
