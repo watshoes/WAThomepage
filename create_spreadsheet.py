e2fd912a840cf4f2e8a1262b18c23cb8337a121e
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2018-09-12 20:44:49 -0400

    changed instance variable

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -5,10 +5,10 @@ class Spreadsheet(object):
     entries = {}
     count = 1
     def add_values(self,values):
-        with open("beta_testers.txt",'a+') as data:
+        with open("beta_testers.txt",'w+') as data:
             entry = values['name1'], values['email1'],values['gender'],values['FavoriteShoe']
-            entries[count] = entry
-            data.write("\nentry {}: {}".format(count,entry))
+            self.entries[self.count] = entry
+            data.write("\nentry {}: {}".format(self.count,entry))
             count += 1
             print(data.read())
             s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
