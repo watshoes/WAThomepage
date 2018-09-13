d884fe0de66f20324ed5ca400a725fe8cf90a734
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2018-09-12 20:46:37 -0400

    count fixed

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -9,7 +9,7 @@ class Spreadsheet(object):
             entry = values['name1'], values['email1'],values['gender'],values['FavoriteShoe']
             self.entries[self.count] = entry
             data.write("\nentry {}: {}".format(self.count,entry))
-            count += 1
+            self.count += 1
             print(data.read())
             s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
             print("operation complete")
