37ffa04c2da93d2d700ad233231924fd745d09ad
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2018-09-12 20:23:31 -0400

    instance variables addded

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -2,11 +2,14 @@ import boto3
 s3 = boto3.resource("s3")
 
 class Spreadsheet(object):
-
+    entries = {}
+    count = 1
     def add_values(self,values):
         with open("beta_testers.txt",'w+') as data:
             entry = values['name1'], values['email1'],values['gender'],values['FavoriteShoe']
-            data.write("\nentry: {}".format( entry))
+            entries[count] = entry
+            data.write("\nentry {}: {}".format(count,entry))
+            count += 1
             print(data.read())
             s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
             print("operation complete")
