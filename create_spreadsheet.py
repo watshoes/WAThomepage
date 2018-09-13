ec36384a63314367f7ee21ec5a38c7e5ffcaa34f
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2018-09-12 20:16:50 -0400

    reading file

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -4,8 +4,9 @@ s3 = boto3.resource("s3")
 class Spreadsheet(object):
 
     def add_values(self,values):
-        with open("beta_testers.txt",'a') as data:
+        with open("beta_testers.txt",'w+') as data:
             entry = values['name1'], values['email1'],values['gender'],values['FavoriteShoe']
             data.write("\nentry: {}".format( entry))
+            print(data.read())
             s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
             print("operation complete")
