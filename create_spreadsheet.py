5cb0b9af28f146a00391d8eb7aea7cd3fcda08d7
Sean Brown <seanbrown@Seans-MBP.fios-router.home>
2018-09-14 18:15:57 -0400

    changed uploading

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -11,5 +11,5 @@ class Spreadsheet(object):
             print(self.entries)
             data.write("\nentry {}: {}".format(self.count,self.entries[self.count]))
             self.count += 1
-            s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
+        s3.meta.client.upload_file('beta_testers.txt', 'visionprocessing', 'beta_testers.txt')
             print("operation complete")
