c1f6d66e89b87bc3c1eec28c9c859ab56f8ba1d2
Sean Brown <seanbrown@Seans-MacBook-Pro.local>
2019-02-25 22:52:15 -0500

    added gmaiConnecotr

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, url_for, send_from_directory, request
 from create_spreadsheet import Spreadsheet
-sheet = Spreadsheet()
+#sheet = Spreadsheet()
+goog = gmailConnector()
 app = Flask(__name__, static_url_path='')
 
 @app.route("/",methods=["POST","GET"])
 def index():
     if request.method == "POST":
-        if 'gender' in request.form:
-            sheet.add_values(request.form)
-            return "{} added".format(request.form["name1"])
+        message = goog.create_message(request.form['email'],'sean@watshoes.co',request.form['name']+ " sending from website",request.form['message'])
+        serv = goog.serv()
+        goog.send_message(serv,"sean",message)
+
     return render_template("index.html")
+
 @app.route("/images/<path:path>")
 def send_images(path):
         return send_from_directory('images',path)
+
 @app.route("/assets/<path:path>")
 def send_assets(path):
     return send_from_directory("assets",path)
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ gmailConnector.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+class gmailConnector():
+    def create_message(self,sender, to, subject, message_text):
+      """Create a message for an email.
+
+      Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+      Returns:
+        An object containing a base64url encoded email object.
+      """
+      message = MIMEText(message_text)
+      message['to'] = to
+      message['from'] = sender
+      message['subject'] = subject
+      return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+    def service(self):
+      creds = None
+      # The file token.pickle stores the user's access and refresh tokens, and is
+      # created automatically when the authorization flow completes for the first
+      # time.
+      if os.path.exists('token.pickle'):
+          with open('token.pickle', 'rb') as token:
+              creds = pickle.load(token)
+      # If there are no (valid) credentials available, let the user log in.
+      if not creds or not creds.valid:
+          if creds and creds.expired and creds.refresh_token:
+              creds.refresh(Request())
+          else:
+              flow = InstalledAppFlow.from_client_secrets_file(
+                  'credentials.json', SCOPES)
+              creds = flow.run_local_server()
+          # Save the credentials for the next run
+          with open('token.pickle', 'wb') as token:
+              pickle.dump(creds, token)
+
+      service = build('gmail', 'v1', credentials=creds)
+      return service
+
+    def send_message(self,service, user_id, message):
+        try:
+            message = (service.users().messages().send(userId=user_id, body=message)
+                       .execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except(errors.HttpError, error):
+            print('An error occurred: %s' % error)
