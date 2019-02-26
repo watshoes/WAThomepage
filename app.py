1bf32e2b4262e84009f3edb5c216cfcc3b1e85d1
Sean Brown <seanbrown@Seans-MacBook-Pro.local>
2019-02-26 18:47:15 -0500

    test

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, url_for, send_from_directory, request
 from create_spreadsheet import Spreadsheet
-#from gmailConnector import GmailConnector
+from gmailConnector import GmailConnector
 #sheet = Spreadsheet()
-#goog = GmailConnector()
+goog = GmailConnector()
 app = Flask(__name__, static_url_path='')
 
 @app.route("/",methods=["POST","GET"])
 def index():
     if request.method == "POST":
-        message = goog.create_message(request.form['email'],'sean@watshoes.co',request.form['name']+ " sending from website",request.form['message'])
-        serv = goog.serv()
-        goog.send_message(serv,"sean",message)
+        print(request.form)
+        # message = goog.create_message(request.form['email'],'sean@watshoes.co',request.form['name']+ " sending from website",request.form['message'])
+        # serv = goog.serv()
+        # goog.send_message(serv,"sean",message)
 
     return render_template("index.html")
 
